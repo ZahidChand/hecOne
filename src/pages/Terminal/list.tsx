75f07251580f0ea3ccc208f1d902dae1afb730b0
Zahid <zahid@webassic.com>
2022-11-03 11:09:37 +0530

    Terminal Name Link Added

--- src/pages/Terminal/list.tsx
+++ src/pages/Terminal/list.tsx
@@ -4,6 +4,7 @@ import {
     useMany,
     CrudFilters,
     useTranslate,
+    useNavigation,
 } from "@pankod/refine-core";
 
 import {
@@ -37,7 +38,9 @@ import { Box, ButtonGroup, Drawer, DrawerBody, DrawerCloseButton, DrawerContent,
 import { SearchOutlined } from "@ant-design/icons";
 import { useRef } from "react";
 import { FilterView } from "../../components/Filter/Filter";
-
+import show from "./show";
+import { color } from "@pankod/refine-mui";
+import "./terminal.css"
 export const GridListView: React.FC<IResourceComponentsProps> = () => {
     const { tableProps, sorter, searchFormProps } = useTable<ITerminal>({
         initialSorter: [
@@ -57,84 +60,97 @@ export const GridListView: React.FC<IResourceComponentsProps> = () => {
         },
     });
     const t = useTranslate();
+    const { show } = useNavigation();
+
     return (
         <>
-        <Form>
-            <Row
-                // {...listProps}
-                gutter={[16, 16]}
-                style={{ background: "#fff", padding: "24px 24px" }}
-            >
-                <div>
-                    <List>
-                        <Table {...tableProps} rowKey="id">
-                            <Table.Column
-                                dataIndex="name"
-                                title="Name"
-                                sorter={{ multiple: 2 }}
-                                defaultSortOrder={getDefaultSortOrder("Name", sorter)}
-                            />
-                            <Table.Column
-                                dataIndex="longitude"
-                                title="Longitude"
-                                sorter={{ multiple: 1 }}
-                            />
-                            <Table.Column
-                                dataIndex="latitude"
-                                title="Latitude"
-                                sorter={{ multiple: 1 }}
-                            />
-                            <Table.Column
-                                dataIndex="id"
-                                title="Terminal Number"
-                                sorter={{ multiple: 2 }}
-                                defaultSortOrder={getDefaultSortOrder("id", sorter)}
-                            />
-                            <Table.Column
-                                dataIndex="created_at"
-                                title="Created At"
-                                render={(value) => <DateField value={value} format="LLL" />}
-                                defaultSortOrder={getDefaultSortOrder("created_at", sorter)}
-                                sorter
-                            />
-                            <Table.Column<ITerminal>
-                                title="Actions"
-                                dataIndex="actions"
-                                render={(_, record) => (
-                                    <Space>
-                                        <EditButton
-                                            hideText
-                                            size="small"
-                                            recordItemId={record.id}
-                                        />
-                                        <ShowButton
-                                            hideText
-                                            size="small"
-                                            recordItemId={record.id}
-                                        />
-                                        <DeleteButton
-                                            hideText
-                                            size="small"
-                                            recordItemId={record.id}
-                                            metaData={{
-                                                fields: [
-                                                    "id",
-                                                    "name",
-                                                    "longitude",
-                                                    "latitude",
-                                                    "created_at",
-                                                ],
-                                            }}
-                                        />
-                                    </Space>
-                                )}
-                            />
-                        </Table>
-                    </List>
-                </div>
-            </Row>
-        </Form>
-    </>
-        
+            <Form>
+                <Row
+                    // {...listProps}
+                    gutter={[16, 16]}
+                    style={{ background: "#fff", padding: "24px 24px" }}
+                >
+                    <div>
+                        <List>
+                            <Table {...tableProps} rowKey="id"
+                                onRow={(record) => {
+                                    return {
+                                        onClick: () => {
+                                            show("HecOne_Terminal", record?.id)
+                                        },
+                                    };
+                                }}
+                            >
+
+                                <Table.Column
+                                    className="link-css"
+                                    dataIndex="name"
+                                    title="Name"
+                                    sorter={{ multiple: 2 }}
+                                    defaultSortOrder={getDefaultSortOrder("Name", sorter)}
+
+                                />
+                                <Table.Column
+                                    dataIndex="longitude"
+                                    title="Longitude"
+                                    sorter={{ multiple: 1 }}
+                                />
+                                <Table.Column
+                                    dataIndex="latitude"
+                                    title="Latitude"
+                                    sorter={{ multiple: 1 }}
+                                />
+                                <Table.Column
+                                    dataIndex="id"
+                                    title="Terminal Number"
+                                    sorter={{ multiple: 2 }}
+                                    defaultSortOrder={getDefaultSortOrder("id", sorter)}
+                                />
+                                <Table.Column
+                                    dataIndex="created_at"
+                                    title="Created At"
+                                    render={(value) => <DateField value={value} format="LLL" />}
+                                    defaultSortOrder={getDefaultSortOrder("created_at", sorter)}
+                                    sorter
+                                />
+                                <Table.Column<ITerminal>
+                                    title="Actions"
+                                    dataIndex="actions"
+                                    render={(_, record) => (
+                                        <Space>
+                                            <EditButton
+                                                hideText
+                                                size="small"
+                                                recordItemId={record.id}
+                                            />
+                                            <ShowButton
+                                                hideText
+                                                size="small"
+                                                recordItemId={record.id}
+                                            />
+                                            <DeleteButton
+                                                hideText
+                                                size="small"
+                                                recordItemId={record.id}
+                                                metaData={{
+                                                    fields: [
+                                                        "id",
+                                                        "name",
+                                                        "longitude",
+                                                        "latitude",
+                                                        "created_at",
+                                                    ],
+                                                }}
+                                            />
+                                        </Space>
+                                    )}
+                                />
+                            </Table>
+                        </List>
+                    </div>
+                </Row>
+            </Form>
+        </>
+
     );
 };
