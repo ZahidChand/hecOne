2cb3f74d9811c01c9c9917595009d3ef82276b34
sanchita <sanchita.p@webassic.com>
2022-11-04 16:18:08 +0530

    changes made

--- src/pages/NetworkConfig/editNetworkConfig.tsx
+++ src/pages/NetworkConfig/editNetworkConfig.tsx
@@ -4,6 +4,7 @@ import { faCheck } from "@fortawesome/free-solid-svg-icons";
 import { Button, Col, Form, List, Row, SaveButton, Select } from "@pankod/refine-antd";
 import { IResourceComponentsProps, useNavigation } from "@pankod/refine-core";
 import { useState } from "react";
+import "./editNetwork.css"
 
 export const EditNetworkConfig: React.FC<IResourceComponentsProps> = () => {
 
@@ -22,116 +23,117 @@ export const EditNetworkConfig: React.FC<IResourceComponentsProps> = () => {
         <>
             <Form
                 // {...formProps}
-                style={{ marginTop: 10, width: "95%" }}
+                className="ant-advanced-search-form"
+                style={{ }}
                 layout="horizontal"
                 initialValues={{
                     isActive: true,
                 }}
             >
+                <div style={{ padding: "10px"}}>
+                    <Row >
+                        <Col style={{ marginInlineStart: "80%" }}>
+                            {isEditMode ?
+                                <Flex gap={16} style={{ cursor: "pointer", float: "right", fontSize: "20px" }}>
+                                    <Flex>
+                                        <CheckOutlined
+                                            onClick={() => { CancelEdit() }}
+                                        />
+                                        <Spacer />
 
-                <Row>
-                    <Col style={{ marginInlineStart: "80%" }}>
-                        {isEditMode ?
-                            <Flex gap={16} style={{ cursor: "pointer", float: "right", fontSize: "20px" }}>
-                                <Flex>
-                                    <CheckOutlined
+                                    </Flex>
+
+                                    <CloseOutlined
                                         onClick={() => { CancelEdit() }}
                                     />
-                                    <Spacer />
-
                                 </Flex>
 
-                                <CloseOutlined
-                                    onClick={() => { CancelEdit() }}
+                                :
+                                <FormOutlined style={{ cursor: "pointer", float: "right", fontSize: "20px" }}
+                                    onClick={() => { editPerform() }}
                                 />
-                            </Flex>
 
-                            :
-                            <FormOutlined style={{ cursor: "pointer", float: "right", fontSize: "20px" }}
-                                onClick={() => { editPerform() }}
-                            />
+                            }
+                        </Col>
+                    </Row>
+                    <Row >
+                        <Col style={{ width: "50%" }}>
+                            <Form.Item
+                                label={("User Name")}
+                                name="userName"
+                                rules={[
+                                    {
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                {isEditMode ?
+                                    <Input defaultValue={'User1'} style={{ float: "right" }} />
+                                    :
+                                    <Text style={{ float: "right", marginTop: "10px" }}>User1</Text>}
+                            </Form.Item>
+                            <Form.Item
+                                label={("Server Port")}
+                                name="serverPort"
+                                rules={[
+                                    {
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                {isEditMode ?
+                                    <Input defaultValue={'1234'} style={{ float: "right" }} />
+                                    :
+                                    <Text style={{ float: "right", marginTop: "10px" }}>1234</Text>}
+                            </Form.Item>
+                            <Form.Item
+                                label={("Server Host")}
+                                name="serverHost"
+                                rules={[
+                                    {
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                {isEditMode ?
+                                    <Input defaultValue={'sr1.host1.com'} style={{ float: "right" }} />
+                                    :
+                                    <Text style={{ float: "right", marginTop: "10px" }}>sr1.host1.com</Text>}
 
-                        }
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Form.Item
-                            label={("User Name")}
-                            name="userName"
-                            rules={[
-                                {
-                                    required: true,
-                                },
-                            ]}
-                        >
-                            {isEditMode ?
-                                <Input defaultValue={'User1'} style={{ float: "right" }} />
-                                :
-                                <Text >User1</Text>}
-                        </Form.Item>
-                        <Form.Item
-                            label={("Server Port")}
-                            name="serverPort"
-                            rules={[
-                                {
-                                    required: true,
-                                },
-                            ]}
-                        >
-                            {isEditMode ?
-                                <Input defaultValue={'1234'} style={{ float: "right" }} />
-                                :
-                                <Text >1234</Text>}
-                        </Form.Item>
-                        <Form.Item
-                            label={("Server Host")}
-                            name="serverHost"
-                            rules={[
-                                {
-                                    required: true,
-                                },
-                            ]}
-                        >
-                            {isEditMode ?
-                                <Input defaultValue={'sr1.host1.com'} style={{ float: "right" }} />
-                                :
-                                <Text >sr1.host1.com</Text>}
+                            </Form.Item>
+                            <Form.Item
+                                label={("Server Protocol")}
+                                name="serverProtocol"
+                                rules={[
+                                    {
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                {isEditMode ?
+                                    <Input defaultValue={'serverProtocol1'} style={{ float: "right" }} />
+                                    :
+                                    <Text style={{ float: "right", marginTop: "10px" }}>serverProtocol1</Text>}
 
-                        </Form.Item>
-                        <Form.Item
-                            label={("Server Protocol")}
-                            name="serverProtocol"
-                            rules={[
-                                {
-                                    required: true,
-                                },
-                            ]}
-                        >
-                            {isEditMode ?
-                                <Input defaultValue={'serverProtocol1'} style={{ float: "right" }} />
-                                :
-                                <Text >serverProtocol1</Text>}
+                            </Form.Item>
 
-                        </Form.Item>
+                            <Form.Item
+                                label={("Keys")}
+                                name="keys"
+                                rules={[
+                                    {
+                                        required: true,
+                                    },
+                                ]}
+                            >
+                                {isEditMode ?
+                                    <Input defaultValue={'abdcefghi'} style={{ float: "right" }} />
+                                    :
+                                    <Text style={{ float: "right", marginTop: "10px" }}>abdcefghi</Text>}
 
-                        <Form.Item
-                            label={("Keys")}
-                            name="keys"
-                            rules={[
-                                {
-                                    required: true,
-                                },
-                            ]}
-                        >
-                            {isEditMode ?
-                                <Input defaultValue={'abdcefghi'} style={{ float: "right" }} />
-                                :
-                                <Text >abdcefghi</Text>}
+                            </Form.Item>
 
-                        </Form.Item>
-
-                        {/* <Form.Item>
+                            {/* <Form.Item>
                             {
                                 isEditMode ?
                                     <Flex>
@@ -152,8 +154,9 @@ export const EditNetworkConfig: React.FC<IResourceComponentsProps> = () => {
                             }
 
                         </Form.Item> */}
-                    </Col>
-                </Row>
+                        </Col>
+                    </Row>
+                </div>
             </Form>
         </>
     )
