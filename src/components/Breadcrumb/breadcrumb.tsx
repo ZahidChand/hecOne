aad0fdda518a4312b5edbb7a7e97720331a1009d
Anup Agarwal <anupagarwal@Anups-MacBook-Pro.local>
2022-11-10 23:54:31 +0530

    fixed link

--- src/components/Breadcrumb/breadcrumb.tsx
+++ src/components/Breadcrumb/breadcrumb.tsx
@@ -1,21 +1,23 @@
 import { Breadcrumb } from "antd"
 import React from "react"
 
-const App: React.FC = () => (
-  <Breadcrumb>
-    <Breadcrumb.Item>
-      <a href={`${process.env.REACT_APP_API}/HecOne_Terminal`}>Terminal</a>
-    </Breadcrumb.Item>
-    <Breadcrumb.Item>
-      <a href={`${process.env.REACT_APP_API}/HecOne_Terminal/show/id`}>
-        Details
-      </a>
-    </Breadcrumb.Item>
-    {/* <Breadcrumb.Item>
+const App: React.FC = () => {
+  return (
+    <Breadcrumb>
+      <Breadcrumb.Item>
+        <a href="/HecOne_Terminal" onClick={() => {}}>
+          Terminal
+        </a>
+      </Breadcrumb.Item>
+      <Breadcrumb.Item>
+        <a href={`/HecOne_Terminal/show/id`}>Details</a>
+      </Breadcrumb.Item>
+      {/* <Breadcrumb.Item>
       <a href={`${process.env.REACT_APP_API}/HecOne_Terminal/edit/id`}>Edit</a>
     </Breadcrumb.Item> */}
-    {/* <Breadcrumb.Item>An Application</Breadcrumb.Item> */}
-  </Breadcrumb>
-)
+      {/* <Breadcrumb.Item>An Application</Breadcrumb.Item> */}
+    </Breadcrumb>
+  )
+}
 
 export default App
